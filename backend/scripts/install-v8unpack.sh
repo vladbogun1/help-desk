655c6588ca39547ea19158c06248fed1a9c14aa3
V_lastelin <vladbogun@gmail.com>
2026-02-12 01:25:47 +0400

    Install git in backend image for v8unpack pip Git URL

--- backend/scripts/install-v8unpack.sh
+++ backend/scripts/install-v8unpack.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-python -m pip install --no-cache-dir --upgrade pip
 python -m pip install --no-cache-dir "git+https://github.com/saby-integration/v8unpack.git"
